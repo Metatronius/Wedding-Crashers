6eb456c550780e53dd9788623ef0c95a571cbcf2
Unknown <plederer93@gmail.com>
2019-06-29 18:26:25 -0500

    Started working on fixing modulization

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public int BaseAttackDamage;
+    public float BaseAttackTime;
+    public float AttackRange;
+
+    private float AttackTime;
+    private float AttackProgress;
+    private bool InProgress;
+    public Vector2 Direction; //(-1, 0) = Left, (1, 0) = Right
+
+    private void DoAttack() // int bulletDamage, int bulletSpeed, Bullet bullet
+    {
+        var bullet = Instantiate(Bullet, this.transform.position + new Vector3(0, .3f * Mathf.Cos(System.DateTime.Now.Millisecond), -1), this.transform.rotation).GetComponent<Bullet>();
+        bullet.Initialize(BulletDamage, BulletSpeed, new Vector2(-1, 0));
+
+        return bullet;
+    }
+}
---
+++ Assets/Scripts/ResourceSpendingButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceSpendingButton : MonoBehaviour
+{
+    private ResourceManager resourceManager;
+    public int Price { get; set; }
+    public int BasePrice;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Price = BasePrice;
+    }
+    void Press(IUpgradable e)
+    {
+
+        if(resourceManager.Spend(Price))
+        {
+            e.Upgrade();
+        }
+    }
+}
